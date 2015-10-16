98f14eda0ec46e494a405c0c10cef6907acaac99
Harrison Statham <harrison.statham@comcast.net>
2015-10-16 19:12:12 -0400

    Initial Commit

---
+++ Assembler/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assembler
+//
+//  Created by Harrison on 10/16/15.
+//  Copyright © 2015 HarrisonStatham. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
